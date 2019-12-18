4765e75f5549f4db6ca2e50207aad2190dbf5850
zhangYing <zy49666@ly.com>
2019-12-18 21:56:49 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 
 import { MyLibModule } from 'my-lib';
+import { FirstLeafletComponent } from './first-leaflet/first-leaflet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FirstLeafletComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/first-leaflet/first-leaflet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-leaflet',
+  templateUrl: './first-leaflet.component.html',
+  styleUrls: ['./first-leaflet.component.css']
+})
+export class FirstLeafletComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
