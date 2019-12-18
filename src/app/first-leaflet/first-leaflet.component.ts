c8a3df1cab5b7cbbf54aadee13eab3bf47caa8de
zhangYing <zy49666@ly.com>
2019-12-18 22:33:24 +0800

    update

--- src/app/first-leaflet/first-leaflet.component.ts
+++ src/app/first-leaflet/first-leaflet.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare const L: any;
 @Component({
   selector: 'app-first-leaflet',
   templateUrl: './first-leaflet.component.html',
@@ -10,6 +10,11 @@ export class FirstLeafletComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
   }
 
 }
