2d7cacffe4bcfd3d329af94a990afdd989c53c3b
zhangying <yingzhang@siemens.com>
2019-12-19 18:06:14 +0800

    update

--- src/app/first-leaflet/first-leaflet.component.ts
+++ src/app/first-leaflet/first-leaflet.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-declare const L: any;
+import * as L from 'leaflet';
 @Component({
   selector: 'app-first-leaflet',
   templateUrl: './first-leaflet.component.html',
@@ -13,8 +13,21 @@ export class FirstLeafletComponent implements OnInit {
     const map = L.map('map').setView([51.505, -0.09], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    L.marker([51.5, -0.09]).addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+
+    // const m = L.map('map-container').setView([36.52, 120.31], 7);
+    // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    //   maxZoom: 18,
+    //   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    //     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    //     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    //   id: 'mapbox.streets'
+    // }).addTo(m);
   }
 
 }
