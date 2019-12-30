9f825eb173a97a011de04cb5d4b9c87d3ffa4b72
zhangying <yingzhang@siemens.com>
2019-12-30 17:38:40 +0800

    update

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 git add .
 git cm -m 'update'
 git push
-expect "Username for 'https://github.com':"
-send "zhangying2345\r"
+expect "Username*"
+send -- "zhangying2345\r"
 expect "Password"
 send "182838a?\r"
